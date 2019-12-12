21cde2e78efb43cc9601fb17963bda392597e2c6
Sandeep Kothari <skothari@webuy.com>
2019-12-12 17:19:17 +0530

    Update CiTest.php

--- CiTest.php
+++ CiTest.php
@@ -17,4 +17,38 @@ public function testString(){
         $this->assertSame(0, count($stack));
 }
 
+
+public function testSample(){
+        $this->assertEquals("Hello","hello");
+}
+
+    public function testEmpty()
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testPush(array $stack)
+    {
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack)-1]);
+        $this->assertNotEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testPush
+     */
+    public function testPop(array $stack)
+    {
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertEmpty($stack);
+    }
+
 }
