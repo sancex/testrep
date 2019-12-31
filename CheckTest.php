473a7b7e61a900b43d64e4729e60e267b888da55
Sandeep Kothari <skothari@webuy.com>
2019-12-31 17:35:18 +0530

    Update CheckTest.php

--- CheckTest.php
+++ CheckTest.php
@@ -10,12 +10,7 @@ Class CheckTest extends TestCase{
 }
 Class Check{
   public function checkDb(){
-    $db="dev_cex_2";
-    $uname="dev_taproot";
-    $pwd="taproot";
-    $conn=mysqli_connect("10.204.2.26",$uname,$pwd,$db);
-    $res = mysqli_query($conn,"select * from boxes limit 1");
-    $row = mysqli_fetch_row($res);
+    $row=[];
     return $row;
   }
 }
